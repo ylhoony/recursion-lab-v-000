3a87a6f670374dc4a7807c04bc7ad4140f2c0cb0
Hoon Lee <yl.hoony@gmail.com>
2018-08-22 00:15:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 // Code your solution here!
 
 function printString(string) {
-  
+  if (!!string.length) {
+    console.log(string[0]);
+    
+    printString(string.substring(1, string))
+  } else {
+    return
+  }
 }
 
